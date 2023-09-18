ba31668c0a9bc99c69f2ce79bf317249f809f8b6
rutlexd <maaks.minecrafter@gmail.com>
2023-09-18 23:13:17 +0300

    Init

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# IN CODDING - 30% </br>
+MVP 
+</br>1. Check name ( relies user text input, create sql-request into name, 
+if name finds ask password , else add name to table USERS and ask to create password)
+</br>2. show list with goods ( sql-request , show list from table GOODS (name , count))
+</br>3. list is clickable, when click on goods open dialog window "U sure?" ,
+if yes ask needed count as number and count this goods -number, if goods not enough say it for user.
+</br>4.Back to choose menu.
--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <string>
 #include "sqlite3.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 using namespace std;
-
+using namespace sf;
 int main() {
+
+
+
+    RenderWindow window(VideoMode(400,600), "main");
+    
+    Font font;
+    font.loadFromFile("Mooli-Regular.ttf");
+    if (!font.loadFromFile("Mooli-Regular.ttf"))
+{
+    cout << "Error to load font" << endl;
+}
+
+    
+
+    RectangleShape button1 (Vector2f (120 , 40));
+
+
+    button1.setFillColor(Color (114, 131, 204));
+    button1.setPosition(Vector2f (200,125));
+
+    RectangleShape button2 (Vector2f (120 , 40));
+
+    button2.setFillColor(Color (114, 131, 204));
+    button2.setPosition(Vector2f (200,325));
+
+    Color color = Color (255, 255, 255, 1);
+
+    
+
+    while (window.isOpen()){
+        Event event;
+        while(window.pollEvent(event)){
+            if(event.type == Event::Closed){
+                window.close();
+            }
+        }
+
+    window.clear(color);
     sqlite3 *db;
     char *errMsg = nullptr;
     string userEnter;
@@ -13,8 +53,40 @@ int main() {
     int rc = sqlite3_open("Database.db", &db);
 
 
+    if (Mouse::isButtonPressed(Mouse::Left)){
+        Vector2i mousePos = Mouse::getPosition(window);
+        if(button1.getGlobalBounds().contains(mousePos.x, mousePos.y)){
+            color = Color (77, 18, 166);
+            const char *sql=
+          "CREATE TABLE numbers(num1 INTEGER PRIMARY KEY,num2,str);"
+          "INSERT INTO numbers VALUES(1,11,'ABC');"
+          "INSERT INTO numbers VALUES(2,22,'DEF');"
+          "INSERT INTO numbers VALUES(3,33,'UVW');"
+          "INSERT INTO numbers VALUES(4,44,'XYZ');" ;
+            rc = sqlite3_exec(db, sql, NULL, NULL, NULL);
+            cout << "Button1 is presed" << endl;
+        }
+    }
+
+    if (Mouse::isButtonPressed(Mouse::Left)){
+        Vector2i mousePos = Mouse::getPosition(window);
+        if (button2.getGlobalBounds().contains(mousePos.x, mousePos.y)){
+            color = Color(62, 122, 86);
+            cout << "Button1 is presed" << endl;
+        }
+    }
+
+
+    window.draw(button1);
+    window.draw(button2);
+
+
+
+
    
     sqlite3_close(db);
+    window.display(); 
+    }
 
     return 0;
 }
