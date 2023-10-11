7d9591e8181c764dc274946aaa09f40d3b165d2a
rutlexd <maaks.minecrafter@gmail.com>
2023-10-11 20:13:16 +0300

    Small update

--- main.cpp
+++ main.cpp
@@ -10,39 +10,39 @@
 using namespace std;
 using namespace sf;
 
-struct USER
+struct User
 {
     string name;
     string password;
     bool successfullyCheck = 0;
 };
 
-struct GOODS
+struct Goods
 {
     vector <string> name;
     vector <string> count;
 };
 
-string deleteSpaceInString (string name);
-static int login_callback(void *unused, int count, char **data, char **columns);
-static int listOfGoods_callback(void *unused, int count, char **data, char **columns);
+string deleteSpaceInString(string name);
+static int loginCallback(void *unused, int count, char **data, char **columns);
+static int listOfGoodsCallback(void *unused, int count, char **data, char **columns);
 static int checkForNull_callback(void *unused, int count, char **data, char **columns);
 int showLogin();
 void showShop ();
 bool loginUser(string inputName, string inputPassword);
-GOODS createListOfGoods();
-void editCount(int button, GOODS goods);
-GOODS updateGoods();
+Goods createGoods();
+void editCount(int button, Goods goods);
+Goods updateGoods();
 Font getFont();
 
 const int WIDTH = 400;
 const int HIGHT = 600;
 const int FRAME = 11;
-const Font FONT = getFont();
+const Font defaultFont = getFont();
 const char *WAYTODB = "src/Database.db";
 
 
-int main() {
+int main(){
 
     if (showLogin() == 1){
         return 0;
@@ -54,57 +54,57 @@ int main() {
 
 int showLogin(){
     
-    RenderWindow window(VideoMode(WIDTH,HIGHT), "login");
+    RenderWindow window(VideoMode(WIDTH, HIGHT), "login");
     
     window.setFramerateLimit(FRAME);
 
     Color swampGreenColor = Color (62, 122, 86);  
-    Color coffeColor = Color (158, 143, 103);
+    Color coffeeColor = Color (158, 143, 103);
 
     RectangleShape nameInputBox (Vector2f (120 , 40));
 
     nameInputBox.setFillColor(Color::White);
-    nameInputBox.setPosition(Vector2f (140,150));
+    nameInputBox.setPosition(Vector2f (140, 150));
     nameInputBox.setOutlineThickness(5);
     nameInputBox.setOutlineColor(Color::Black);
 
-    RectangleShape passwordInputBox (Vector2f (120 , 40));
+    RectangleShape passwordInputBox (Vector2f (120, 40));
 
     passwordInputBox.setFillColor(Color::White);
-    passwordInputBox.setPosition(Vector2f (140,240));
+    passwordInputBox.setPosition(Vector2f (140, 240));
     passwordInputBox.setOutlineThickness(5);
     passwordInputBox.setOutlineColor(Color::Black);
 
     Text nameInputText;
-   
-    nameInputText.setFont(FONT);
+
+    nameInputText.setFont(defaultFont);
     nameInputText.setFillColor(Color::Black);
-    nameInputText.setPosition(Vector2f (140,150));
+    nameInputText.setPosition(Vector2f (140, 150));
     nameInputText.setCharacterSize(14);
 
     Text passwordInputText;
 
     passwordInputText = nameInputText;
-    passwordInputText.setPosition(Vector2f(140,240));
+    passwordInputText.setPosition(Vector2f(140, 240));
 
     Text sentNameText;
 
     sentNameText.setString("Enter");
-    sentNameText.setFont(FONT);
+    sentNameText.setFont(defaultFont);
     sentNameText.setFillColor(Color::Black);
     sentNameText.setPosition(Vector2f (160, 340));
 
     RectangleShape sentNameButton;
 
     sentNameButton.setSize(Vector2f (120, 40));
-    sentNameButton.setFillColor(coffeColor);
-    sentNameButton.setPosition(Vector2f (140,340));
+    sentNameButton.setFillColor(coffeeColor);
+    sentNameButton.setPosition(Vector2f (140, 340));
     sentNameButton.setOutlineThickness(5);
     sentNameButton.setOutlineColor(Color::Black);
 
     Text inputError;
 
-    inputError.setFont(FONT);
+    inputError.setFont(defaultFont);
     inputError.setFillColor(Color::Red);
     inputError.setString("Error, try again");
     inputError.setPosition(Vector2f (120, 200));
@@ -112,7 +112,7 @@ int showLogin(){
 
     Text nameEnter;
 
-    nameEnter.setFont(FONT);
+    nameEnter.setFont(defaultFont);
     nameEnter.setFillColor(Color::Black);
     nameEnter.setString("Name");
     nameEnter.setPosition(Vector2f (30, 150));
@@ -120,7 +120,7 @@ int showLogin(){
 
     Text passwordEnter;
 
-    passwordEnter.setFont(FONT);
+    passwordEnter.setFont(defaultFont);
     passwordEnter.setFillColor(Color::Black);
     passwordEnter.setString("Password");
     passwordEnter.setPosition(Vector2f (30, 240));
@@ -188,7 +188,7 @@ int showLogin(){
                     }
                     isInputError = false;
                 }
-                else{
+                else {
                     isInputError = true;
                 }
                 
@@ -222,9 +222,9 @@ int showLogin(){
 }
 
 
-void showShop () {
+void showShop (){
 
-    RenderWindow shop(VideoMode(WIDTH,HIGHT), "shop");
+    RenderWindow shop(VideoMode(WIDTH, HIGHT), "shop");
 
     shop.setFramerateLimit(FRAME);
 
@@ -237,7 +237,7 @@ void showShop () {
 
     Text plus;
 
-    plus.setFont(FONT);
+    plus.setFont(defaultFont);
     plus.setFillColor(Color::Black);
     plus.setCharacterSize(24);
     plus.setString("+");
@@ -249,11 +249,11 @@ void showShop () {
     int x = 20;
     int y = 20;
     
-    GOODS goods = createListOfGoods(); 
+    Goods goods = createGoods(); 
 
     Text product;
 
-    product.setFont(FONT);
+    product.setFont(defaultFont);
     product.setFillColor(Color::Black);
     product.setCharacterSize(18);
 
@@ -320,10 +320,10 @@ void showShop () {
             Vector2i MousePos = Mouse::getPosition(shop);
             for (auto button = 0; button < opButtons.size(); button++){
                 if (opButtons[button].getGlobalBounds().contains(MousePos.x, MousePos.y)){ 
-                    
+
                     editCount(button, goods);
                     goods = updateGoods();
-
+                    
                     for (auto i = 0; i < goods.name.size(); i++){
                         countProd[i].setString(goods.count[i]);
                     }
@@ -357,7 +357,7 @@ string deleteSpaceInString (string name){
 }
 
 
-bool loginUser(string inputName, string inputPassword) {
+bool loginUser(string inputName, string inputPassword){
     string name = deleteSpaceInString(inputName);
     string password = deleteSpaceInString(inputPassword);
 
@@ -367,18 +367,18 @@ bool loginUser(string inputName, string inputPassword) {
 
     int rc = sqlite3_open(WAYTODB, &db);
 
-    USER user;
+    User user;
     user.name = name;
     user.password = password;
 
     rc = sqlite3_exec(db, sql.c_str(), NULL, NULL, NULL);
     
     if (rc != SQLITE_OK ){
-        sqlite3_exec(db, "SELECT * FROM USERS", login_callback, &user, NULL);
+        sqlite3_exec(db, "SELECT * FROM USERS", loginCallback, &user, NULL);
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
     } 
-    else{
+    else {
         user.successfullyCheck = true;
         fprintf(stdout, "Records created successfully\n");
     }
@@ -387,16 +387,17 @@ bool loginUser(string inputName, string inputPassword) {
 
     if (user.successfullyCheck){
         return true;
-    }else{
+    }
+    else {
         return false;
     }
 
 }
 
 
-static int login_callback(void *unused, int count, char **data, char **columns){                   
+static int loginCallback(void *unused, int count, char **data, char **columns){                   
 
-    USER *user = static_cast<USER*>(unused);
+    User *user = static_cast<User*>(unused);
     
     if (user->name == data[0] && user->password == data[1]){
         user -> successfullyCheck = true;
@@ -406,20 +407,20 @@ static int login_callback(void *unused, int count, char **data, char **columns){
 }
 
 
-GOODS createListOfGoods() {
+Goods createGoods(){
 
     sqlite3 *db;
-    GOODS goods;
+    Goods goods;
 
     sqlite3_open(WAYTODB, &db);
-    sqlite3_exec(db, "SELECT * FROM GOODS", listOfGoods_callback, &goods, NULL);
+    sqlite3_exec(db, "SELECT * FROM GOODS", listOfGoodsCallback, &goods, NULL);
     sqlite3_close(db);
 
     return goods;
 }
 
 
-void editCount(int button, GOODS goods){
+void editCount(int button, Goods goods){
 
     sqlite3 *db;
     string sql;
@@ -428,7 +429,7 @@ void editCount(int button, GOODS goods){
     if (button % 2 == 0){ 
         sql = "UPDATE GOODS SET count = count + 1 WHERE name = '"+ goods.name[button / 2] + "';";
     }
-    else{
+    else {
         if (goods.count[button / 2] != '0'){
             sql = "UPDATE GOODS SET count = count - 1 WHERE name = '"+ goods.name[button / 2] + "';";
         }
@@ -438,24 +439,27 @@ void editCount(int button, GOODS goods){
 }
 
 
-GOODS updateGoods(){
-    GOODS goods;
+Goods updateGoods(){
+    Goods goods;
     sqlite3 *db;
     sqlite3_open(WAYTODB, &db);
 
-    sqlite3_exec(db, "SELECT * FROM GOODS", listOfGoods_callback, &goods, NULL);
+    sqlite3_exec(db, "SELECT * FROM GOODS", listOfGoodsCallback, &goods, NULL);
     sqlite3_close(db);
 
     return goods;
 }
-static int listOfGoods_callback(void *unused, int count, char **data, char **columns){      
+
+
+static int listOfGoodsCallback(void *unused, int count, char **data, char **columns){      
   
-    GOODS *goods = static_cast<GOODS*>(unused);
+    Goods *goods = static_cast<Goods*>(unused);
     goods -> name.push_back(data[0]);
     goods -> count.push_back(data[1]);
     
     return 0;
 }
+
 
 Font getFont(){ 
 
